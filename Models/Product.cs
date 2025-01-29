3f06ca0afa3aaac8f0eaf0c3066af51a50fd1afd
Nikita <nikita.korolev.14.08.2005@mail.ru>
2025-01-29 14:13:37 +0500

    gitignore

--- Models/Product.cs
+++ Models/Product.cs
@@ -8,7 +8,7 @@ namespace Test1.Models
 {
     public class Product
     {
-    [JsonPropertyName("IdProduct")] 
+        [JsonPropertyName("IdProduct")] 
         public int id {get; set;}
         [JsonPropertyName("NameProduct")]
         public string? name {get; set;}
